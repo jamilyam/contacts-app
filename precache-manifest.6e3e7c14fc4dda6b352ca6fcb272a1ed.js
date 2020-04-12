41f2b857d30253ddaeaa7f12df0a023a1e91abc9
Jamilya <jamilyam312@gmail.com>
2020-04-12 20:48:14 +0600

    Updates

---
+++ precache-manifest.6e3e7c14fc4dda6b352ca6fcb272a1ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fcdc20875cbd74c78df4a3763c6645",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "f879c408630b9c81cde8",
+    "url": "/contacts-app/static/js/2.79438e7e.chunk.js"
+  },
+  {
+    "revision": "eb37f4d2a396adc6b8235341203d77ed",
+    "url": "/contacts-app/static/js/2.79438e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b7d8c3716b374f6edd",
+    "url": "/contacts-app/static/js/main.8e664049.chunk.js"
+  },
+  {
+    "revision": "46978999a4279c631d22",
+    "url": "/contacts-app/static/js/runtime-main.bcae1327.js"
+  }
+]);
